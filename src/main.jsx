5a313529b97452304da5b619873b0f28a92b15d7
unknown <lecommerce4.0@gmail.com>
2025-10-09 09:33:28 +0200

    Fix: Correct base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// On importe HashRouter au lieu de BrowserRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './contexts/CartContext';
-import { AudioProvider } from './contexts/AudioContext'; // <-- Importer
+import { AudioProvider } from './contexts/AudioContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AudioProvider> {/* <-- Entourer l'application */}
+    {/* On remplace BrowserRouter par HashRouter */}
+    <HashRouter>
+      <AudioProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AudioProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
